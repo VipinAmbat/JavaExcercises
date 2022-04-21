b26a5a8943994c0c127d085a58bb19b343de985a
Ambat Vipin <ambat.vipu@gmail.com>
2022-04-21 14:38:52 +0530

    Java program for arithmatic

---
+++ Arithmatic.java
@@ -0,0 +1,25 @@
+// Program to add,subtract,multiply and devide numbers
+
+public class Arithmatic
+{
+	public static void main(String[] ar)
+	{
+		int a,b,c;
+		
+		a = 10;
+		b = 20;
+		
+		c= a+b;
+		System.out.print("Sum of two nos\n" +c);
+		c= b-a;
+		System.out.print("\nSubtarction of two nos\n" +c);
+		c= b*a;
+		System.out.print("\nMultiplication of two nos\n" +c);
+		c = b/a;
+		System.out.print("\nDivision of two nos\n" +c);
+		c = b%a;
+		System.out.print("\nModule div of two nos\n" +c);
+		
+	}
+	
+}
