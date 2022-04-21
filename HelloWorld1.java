7fef3b47bf63e06a01397a72f21e2351da326751
Ambat Vipin <ambat.vipu@gmail.com>
2022-04-21 14:29:26 +0530

    Java program to print text in next line

---
+++ HelloWorld1.java
@@ -0,0 +1,12 @@
+
+// Program to print text in next line
+public class HelloWorld1
+{
+	public static void main(String[] ar)
+	{
+		System.out.print("Hello\n");
+		System.out.println("Ambat Vipin");
+	}
+	
+}
+
